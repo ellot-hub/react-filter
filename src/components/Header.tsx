9181bdfdc846087681509ec8614f65da10033cf6
김서희 <seohee@cocen.com>
2023-08-11 14:32:53 +0900

    feat: recoil - toggle 연동

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
 import "styles/Header.css";
 import {useLocation} from "react-router-dom";
+import {useRecoilState} from "recoil";
+import {darkModeState} from "state/Atom";
 
 const Header = () => {
     const path = useLocation().pathname;
-    const pageName = path === "/list" ? "여행 리스트" : ""
+    const pageName = path === "/list" ? "여행 리스트" : "";
+    const [darkMode, setDarkMode] = useRecoilState<boolean>(darkModeState);
+
+    const changeToggle = () => {
+        setDarkMode(!darkMode)
+    }
 
     return (
         <>
@@ -11,10 +18,13 @@ const Header = () => {
                 <div className="Header__Title">
                     <span className="Header__PageName">{pageName}</span>
                 </div>
-                <input type="checkbox" id="toggle" hidden />
+                <input type="checkbox" id="darkMode" hidden defaultChecked={darkMode} />
                 <label
-                    htmlFor="toggle"
+                    htmlFor="darkMode"
                     className="Header__Toggle"
+                    onClick={
+                        changeToggle
+                    }
                 >
                     <span className="Header__Button"></span>
                 </label>
