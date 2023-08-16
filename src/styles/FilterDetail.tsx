59130ef3b8ac576ada61029ea418117d5406f1cb
김서희 <seohee@cocen.com>
2023-08-16 14:57:42 +0900

    fix: FilterDetail.css -> FilterDetail.tsx 변경

--- src/styles/FilterDetail.tsx
+++ src/styles/FilterDetail.tsx
@@ -1,16 +1,13 @@
-ul {
-    margin: 0;
-    padding: 0;
-    list-style-type: none;
-}
-.FilterDetail {
+import { styled } from "styled-components";
+
+export const DetailPos = styled.div`
     position: fixed;
     width: 420px;
     top: 110px;
     height: calc(100vh - 110px);
     background-color: rgba(0,0,0,.5);
-}
-.FilterDetail__box {
+`;
+export const DetailBox = styled.div`
     width: 100vw;
     max-width: 420px;
     height: 200px;
@@ -20,18 +17,21 @@ ul {
     display: flex;
     flex-wrap: wrap;
     flex-direction: column;
-}
-.FilterDetail__box--close {
+`;
+export const DetailBoxClose = styled.img`
     width: 30px;
     margin-left: auto;
-}
-.FilterDetail__list {
+`
+export const DetailList = styled.ul`
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
     display: flex;
     flex-wrap: wrap;
     overflow: auto;
     gap: 20px;
-}
-.FilterDetail__list--item {
+`
+export const DetailItem = styled.li`
     flex-basis: 33.33%;
     border: 1px solid #c5c8ce;
     font-size: 14px;
@@ -39,4 +39,4 @@ ul {
     display: inline-flex;
     align-items: center;
     justify-content: center;
-}
+`
